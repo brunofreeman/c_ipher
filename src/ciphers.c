f0bd2316f1bcf316c6b137f9c94dbb5afba9416f
brunofreeman <brunofreeman21@gmail.com>
2020-07-16 00:16:26 -0400

    made Atbash branchless

--- src/ciphers.c
+++ src/ciphers.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "ciphers.h"
 
 char* atbash(const char* plaintext) {
     size_t len = strlen(plaintext);
     char* ciphertext = malloc(len * sizeof(char));
     for (size_t i = 0; i < len; i++) {
-        if ('A' <= plaintext[i] && plaintext[i] <= 'Z') {
-            ciphertext[i] = 'A' + 'Z' - plaintext[i];
-        } else if ('a' <= plaintext[i] && plaintext[i] <= 'z') {
-            ciphertext[i] = 'a' + 'z' - plaintext[i];
-        } else {
-            ciphertext[i] = plaintext[i];
-        }
+        bool upper = 'A' <= plaintext[i] && plaintext[i] <= 'Z';
+        bool lower = 'a' <= plaintext[i] && plaintext[i] <= 'z';
+        ciphertext[i] = (upper) * ('A' + 'Z' - plaintext[i]) +
+                        (lower) * ('a' + 'z' - plaintext[i]) +
+                        (!upper && !lower) * plaintext[i];
     }
     return ciphertext;
 }
