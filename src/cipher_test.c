c30863e10928fcdda644015d7ddad3e6076d1e04
brunofreeman <brunofreeman21@gmail.com>
2020-07-22 22:13:27 -0400

    started redesign of testing system

--- src/cipher_test.c
+++ src/cipher_test.c
@@ -1,93 +1,195 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "ciphers.h"
 
-#define START_TEST_ID "#START_TEST\n"
-#define END_TEST_ID "#END_TEST"
-#define MAX_LEN_ID "#MAX_LEN\n"
-#define PT_ID "#PT\n"
-#define CT_ID "#CT\n"
+#define NUM_TESTS 8
+#define TEST_NAME_LEN 17
+
+#define DIR_EXT_NULL_LEN (7 + 4 + 1)
+#define TEST_DIR "./test/"
+#define TEST_EXT ".txt"
+
+#define ARGS_TOKEN "???"
+
+enum read_mode_e {
+    ENTRY_INFO = 0,
+    INPUT      = 1,
+    OUTPUT     = 2,
+    FAILURE    = 3
+};
+
+enum cipher_e {
+    AFFINE   = 0,
+    ATBASH   = 1,
+    CAESAR   = 2,
+    VIGENERE = 3
+};
+
+enum cipher_direction_e {
+    ENCRYPTION = 0,
+    DECRYPTION = 1
+};
 
 /*
- * Reads the test's max string length.
- * @return 0 on a failure, the designated max_len otherwise
+ * @member info needs to be freed
  */
-ssize_t get_max_len(char* line_buf, size_t* line_buf_size, FILE* file, ssize_t* line_size) {
-    // check first line for #MAX_LEN
-    *line_size = getline(&line_buf, line_buf_size, file);
+typedef struct {
+    char** info;
+    bool success;
+} case_info_t;
 
-    if (*line_size == EOF || strcmp(line_buf, MAX_LEN_ID) != 0) return 0;
+/*
+ * @member text needs to be freed
+ */
+typedef struct {
+    char* entry;
+    bool success;
+} entry_t;
 
-    // read in max_len, will be 0 on failure
-    *line_size = getline(&line_buf, line_buf_size, file);
-    return strtoul(line_buf, NULL, 10);
-}
+typedef struct {
+    enum cipher_e cipher;
+    enum cipher_direction_e cipher_direction;
+    size_t max_text_len;
+    size_t num_args;
+    size_t max_arg_len;
+    bool success;
+} test_info_t;
+
+typedef struct {
+    bool passed;
+} test_result_t;
+
+test_info_t read_test_info(char* line_buf, size_t* line_buf_size, FILE* file, ssize_t* line_size);
+
+case_info_t parse_entry_info(char* line_buf, size_t num_args);
+
+entry_t read_input_output(char* line_buf, size_t* line_buf_size, FILE* file, ssize_t* line_size, bool input);
+
+test_result_t conduct_test(char* input, char* output, enum cipher_e cipher, enum cipher_direction_e cipher_direction);
+
+void print_test_result(size_t count, size_t passed, enum cipher_e cipher, enum cipher_direction_e cipher_direction);
 
 int main(int argc, char** argv) {
+
     printf("Testing...\n");
 
-    FILE* file = fopen("./def/atbash", "r");
-    if (!file) return EXIT_FAILURE;
+    char test_names[NUM_TESTS][TEST_NAME_LEN] = {
+              "affine_encrypt",   "affine_decrypt",
+              "atbash_encrypt",   "atbash_decrypt",
+              "caesar_encrypt",   "caesar_decrypt",
+            "vigenere_encrypt", "vigenere_decrypt"
+    };
 
-    char* line_buf = NULL;
-    size_t line_buf_size = 0;
-    ssize_t line_size;
+    for (size_t i = 0; i < NUM_TESTS; i++) {
 
-    line_size = getline(&line_buf, &line_buf_size, file);
-    if (line_size == EOF || strcmp(line_buf, START_TEST_ID) != 0) {
-        fclose(file);
-        return EXIT_FAILURE;
-    }
+        char file_name[TEST_NAME_LEN + DIR_EXT_NULL_LEN];
+        snprintf(file_name, sizeof(file_name), "%s%s%s", TEST_DIR, test_names[i], TEST_EXT);
 
-    size_t max_len = get_max_len(line_buf, &line_buf_size, file, &line_size);
+        FILE* file = fopen(file_name, "r");
+        if (!file) return EXIT_FAILURE;
 
-    if (max_len == 0) {
+        char* line_buf = NULL;
+        size_t line_buf_size = 0;
+        ssize_t line_size;
+
+        test_info_t test_info = read_test_info(line_buf, &line_buf_size, file, &line_size);
+        if (!test_info.success) {
+            fclose(file);
+            free(line_buf);
+            return EXIT_FAILURE;
+        }
+
+        char*  input = malloc((test_info.max_text_len + 1) * sizeof(char));
+        char* output = malloc((test_info.max_text_len + 1) * sizeof(char));
+
+        enum read_mode_e read_mode = ENTRY_INFO;
+
+        case_info_t entry_info;
+        entry_t input_read;
+        entry_t output_read;
+
+        size_t test_count   = 0;
+        size_t tests_passed = 0;
+
+        bool reading = true;
+
+        while (reading) switch (read_mode) {
+            case ENTRY_INFO: {
+                line_size = getline(&line_buf, &line_buf_size, file);
+                if (line_size == EOF) {
+                    reading = false;
+                } else {
+                    entry_info = parse_entry_info(line_buf, test_info.num_args);
+                    if (!entry_info.success) {
+                        read_mode = FAILURE;
+                        reading = false;
+                    } else {
+                        read_mode = INPUT;
+                    }
+                }
+                break;
+            } case INPUT: {
+                input_read = read_input_output(line_buf, &line_buf_size, file, &line_size, true);
+                if (!input_read.success) {
+                    read_mode = FAILURE;
+                    reading = false;
+                } else {
+                    read_mode = OUTPUT;
+                }
+                break;
+            } case OUTPUT: {
+                output_read = read_input_output(line_buf, &line_buf_size, file, &line_size, true);
+                if (!output_read.success) {
+                    read_mode = FAILURE;
+                    reading = false;
+                } else {
+                    test_result_t test_result = conduct_test(input, output, test_info.cipher, test_info.cipher_direction);
+                    test_count++;
+                    if (test_result.passed) {
+                        tests_passed++;
+                    } else {
+                        // TODO: add more info
+                        printf("Case failed!\n");
+                    }
+                    read_mode = ENTRY_INFO;
+                }
+                break;
+            } default: {
+                read_mode = FAILURE;
+                reading = false;
+            }
+        }
+
         fclose(file);
         free(line_buf);
-        return EXIT_FAILURE;
+        free(input);
+        free(output);
+
+        if (read_mode == FAILURE) return EXIT_FAILURE;
+        else print_test_result(test_count, tests_passed, test_info.cipher, test_info.cipher_direction);
     }
 
-    char* plaintext = malloc(max_len * sizeof(char));
-    char* ciphertext = malloc(max_len * sizeof(char));
+    return EXIT_SUCCESS;
+}
 
-    int read_mode = 1;
+test_info_t read_test_info(char* line_buf, size_t* line_buf_size, FILE* file, ssize_t* line_size) {
 
-    while (line_size != EOF) {
-        line_size = getline(&line_buf, &line_buf_size, file);
-        if (line_size > max_len) {
-            read_mode = -1;
-            break;
-        }
-        if (strcmp(line_buf, END_TEST_ID) == 0) {
-            read_mode = 0;
-            break;
-        }
-        switch (read_mode++) {
-            case 2:
-                strncpy(plaintext, line_buf, line_size - 1);
-                break;
-            case 4:
-                strncpy(ciphertext, line_buf, line_size - 1);
-                printf("%20s: %s\n", "Plaintext", plaintext);
-                printf("%20s: %s\n", "Correct ciphertext", ciphertext);
-                char* generated_ct = atbash_encrypt(plaintext);
-                printf("%20s: %s\n", "Generated ciphertext", generated_ct);
-                free(generated_ct);
-                memset(&plaintext[0], '\0', sizeof(plaintext));
-                memset(&ciphertext[0], '\0', sizeof(ciphertext));
-                read_mode = 1;
-                break;
-        }
-    }
+}
 
-    fclose(file);
+case_info_t parse_entry_info(char* line_buf, size_t num_args) {
 
-    free(line_buf);
-    line_buf = NULL;
+}
 
-    free(plaintext);
-    free(ciphertext);
+entry_t read_input_output(char* line_buf, size_t* line_buf_size, FILE* file, ssize_t* line_size, bool input) {
 
-    return read_mode == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+test_result_t conduct_test(char* input, char* output, enum cipher_e cipher, enum cipher_direction_e cipher_direction) {
+
+}
+
+void print_test_result(size_t count, size_t passed, enum cipher_e cipher, enum cipher_direction_e cipher_direction) {
+
 }
